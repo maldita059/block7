6c8e61f5c4dd3ecc2da1e4a18dcfad4f490334a3
maldita059 <58021619+maldita059@users.noreply.github.com>
2023-05-19 21:44:10 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+//The exercise wants to display a string that says
+//"You have recieved this message because you have been chosen
+//to open an important vault. Here is the secret combination: "
+console.log("You have received this message because you have been"); 
+console.log("chosen to open an important vault.");
+console.log("Here is the secret combination: ");
+
+//Assign three variables each variable contains corresponding
+//results of calculation using unique arithmetic
+// operators +-*/.  Each must equal one of the three codes
+//in the combination. 
+
+let comNum1 = 5 + 5,comNum2 = 50 - 10,comNum3 = 3 * 13;
+
+
+//create a dialog box displaying the vault codes and the text if using
+//HTNL and a script tag or create a popup dialog.
+console.log("Combination 1 is: " + comNum1);
+console.log("Combination 2 is: " + comNum2);
+console.log("Combination 3 is: " + comNum3);
